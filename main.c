c5d0820f5b8847bacbe71cf0b9e44a0594d07c90
mvn23 <schopdiedwaas@gmail.com>
2017-12-29 10:54:36 +0100

    findKbdPath now working.

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   main.c
  * Author: milan
  *
@@ -7,23 +7,57 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <linux/limits.h>
 #include <sys/types.h>
 #include <dirent.h>
 
+//Find keyboard event device by name
+void findKbdPath(char* name, char* path) {
+    FILE *fName;
+    DIR *inputDir;
+    char fContents[strlen(name) + 1];
+    int i;
+    struct dirent *entp;
+    inputDir = opendir("/sys/class/input");
+    while (entp = readdir(inputDir)) {
+        if (strncmp(entp->d_name, "event", 5) == 0) {
+            strncpy(path, "", 1);
+            strncat(path, "/sys/class/input/", 18);
+            strncat(path, entp->d_name, 8);
+            strncat(path, "/device/name", 13);
+            fName = fopen(path, "r");
+            if (fName != NULL) {
+                strncpy(fContents, "", 1);
+                for ( i = 0; i < strlen(name) + 1; i++ ) {
+                    int c = fgetc(fName);
+                    if ( c == 10 || feof(fName)) {
+                        i--;
+                        break;
+                    }
+                   fContents[i] = c;
+                }
+                fContents[i+1] = 0;
+                fclose(fName);
+                if ( strncmp(fContents, name, strlen(name) + 1) == 0 ) {
+                    strncpy(path, "/dev/input/", 12); 
+                    strncat(path, entp->d_name, 8);
+                    return;
+                }
+            }
+        }
+    }
+    return;
+}
+
 /*
- * 
+ *
  */
 int main(int argc, char** argv) {
     char kbdName[] = "AT Translated Set 2 keyboard";
     char ledPath[] = "/sys/class/leds/asus::kbd_backlight/";
-    char kbdPath[] = findKbdPath(kbdName);
-    
+    char kbdPath[PATH_MAX];
+    findKbdPath(kbdName, kbdPath);
+    printf("Keyboard path: %s", kbdPath);
     return (EXIT_SUCCESS);
-}
-
-char *findKbdPath(char* name) {
-    DIR *inputDir;
-    struct dirent *entp;
-    inputDir = opendir(name);
-    
 }
